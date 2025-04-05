275470e29c627ecf2fb4915cf25e1bd2d5de891f
Minh Le <lennminh@gmail.com>
2025-04-05 13:44:46 -0400

    expo set up

---
+++ mobile/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>minh le hehe</Text>
+    </View>
+  );
+}
