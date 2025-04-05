d27ee5e9b5635a66c7567712287c92457f4c92fa
Minh Le <lennminh@gmail.com>
2025-04-05 14:59:21 -0400

    root layout setup

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,11 +1,13 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text style={styles.title}>minh le hehe</Text>
+
+      <Link href="/(auth)/signup">Signup page</Link>
+      <Link href="/(auth)">Login page</Link>
     </View>
   );
 }
