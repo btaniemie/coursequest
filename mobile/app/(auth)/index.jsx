570d0901794908a08d3a6647c539456ba5c021f6
Minh Le <lennminh@gmail.com>
2025-04-05 19:00:37 -0400

    navigation and tabs set up

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -1,18 +1,21 @@
-import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
 import styles from "../../assets/styles/login.styles";
 import { useState } from 'react';
 import COLORS from '../../constants/colors';
 import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
+import { useAuthStore } from '../../store/authStore';
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
 
-    const handleLogin = () => {
+    const { user, isLoading, login } = useAuthStore();
 
+    const handleLogin = async () => {
+        const result = await login(email, password);
+        if (!result.success) Alert.alert("Error", result.error);
     }
   return (
     <View style={styles.container}>
--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -36,5 +36,50 @@ export const useAuthStore = create((set) => ({
             set({ isLoading: false });
             return { successs: false, error: error.message };
         }
+    },
+    checkAuth: async () => {
+        try {
+            const token = await AsyncStorage.getItem("token");
+            const userJson = await AsyncStorage.getItem("user");
+            const user = userJson ? JSON.parse(userJson) : null;
+            set({ token, user });
+        } catch (error) {
+            console.log("Auth check failed", error);
+        }
+    },
+
+    login: async (email, password) => {
+        set({ isLoading: true });
+
+        try {
+            const response = await fetch("https://coursequest-backend.onrender.com/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                }),
+            });
+
+            const data = await response.json();
+            if (!response.ok) throw new Error(data.message || "Something is wrong");
+
+            await AsyncStorage.setItem("user", JSON.stringify(data.user));
+            await AsyncStorage.setItem("token", data.token);
+
+            set({token: data.token, user: data.user, isLoading: false });
+            return { success: true };
+        } catch (error) {
+            set({ isLoading: false });
+            return { successs: false, error: error.message };
+        }
+    },
+
+    logout: async () => {
+        await AsyncStorage.removeItem("token");
+        await AsyncStorage.removeItem("user");
+        set({ token: null, user: null });
     }
 }));
